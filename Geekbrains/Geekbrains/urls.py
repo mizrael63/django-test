58438213af4802529255ee46e3a29a99897e9ad9
mizrael63 <41732636+mizrael63@users.noreply.github.com>
2019-04-26 19:41:31 +0500

    homework2
    optimization to the glory of Django

--- Geekbrains/Geekbrains/urls.py
+++ Geekbrains/Geekbrains/urls.py
@@ -14,14 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main),
-    path('products/', mainapp.products),
-    path('contacts/', mainapp.contacts),
-    path('common/', mainapp.common),
+    path('', mainapp.main, name='main_page'),
+    path('product/', mainapp.products, name='products'),
+    path('contacts/', mainapp.contacts, name='contacts'),
+    path('common/', mainapp.common, name='common'),
     path('admin/', admin.site.urls),
-
+#    url(r'', mainapp.main),
+#    url(r'^products', mainapp.products),
+#    url(r'^contacts', mainapp.contacts),
+#    url(r'^admin/', admin.site.urls),
 ]
--- Geekbrains/mainapp/views.py
+++ Geekbrains/mainapp/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {'user': {'name': 'иван'}, 'array' : [1,2,3,4,5]}
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
     return render(request, 'mainapp/products.html')
 
 def contacts(request):
-    return render(request, 'mainapp/contacts.html')
+    context_c = {'form': {'Address: Russia, N-Tagil city', 'Street: Lenina', 'e-mail: pochta@mail.ru',
+                          'phone: 89900000001',
+                          }}
+    return render(request, 'mainapp/contacts.html', context_c)
 
 def common(request):
     return render(request, 'common/index.html')
